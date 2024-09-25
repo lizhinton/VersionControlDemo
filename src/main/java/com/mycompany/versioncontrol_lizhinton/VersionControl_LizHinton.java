855bc810a3583139b20b4ecbe21ba0c36e954637
hinto <hinto@192.168.0.38>
2024-09-25 15:26:34 -0500

    Initial commit

---
+++ src/main/java/com/mycompany/versioncontrol_lizhinton/VersionControl_LizHinton.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.versioncontrol_lizhinton;
+
+/**
+ *
+ * @author hinto
+ */
+public class VersionControl_LizHinton {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
